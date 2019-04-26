45191b45bb62c57bd96590ee75342b55e7136192
Rene Cocom <fasnimo@gmail.com>
2019-04-26 01:02:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  i = 0 
+  empty_array = []
+  while i < array.length
+  empty_array.push (yield array[i])
+  i += 1
+end
+my_collect(array) {|name|
+  name.split(" ").first}
+end 
 
